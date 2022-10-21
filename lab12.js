fcf7e1189dc9ed059a33949c0a9d1f5d1ba472bc
bachinasriharika <110109962+bachinasriharika@users.noreply.github.com>
2022-10-21 18:47:52 +0530

    Create lab12.js

---
+++ lab12.js
@@ -0,0 +1,28 @@
+import React,{useState,useEffect} from 'react';
+const Windowwidth=()=>{
+    const [WindowWidthsize,setWindowwidthsize]=useState(0);
+    useEffect(()=>{
+        function handleResize(e){
+            const{width}=document.body.getBoundingClientRect();
+            setWindowwidthsize(Math.ceil(width));
+        }
+        window.addEventListener('resize',handleResize);
+        return()=>window.removeEventListener('resize',handleResize);
+    },[]);
+    return(
+        <h1>
+            The window size {WindowWidthsize} Pixels
+        </h1>
+    )
+    }
+export default
+------------
+import Windowwidth from "./Windowwidth.js";
+  function App() {
+    return(
+   <div>
+    <Windowwidth/>
+   </div>
+    )
+  }
+export default App;
